1c067d46c08148f43954fea48758a7dcc88f1571
yuyarooneytrigger13 <yuya.rooney.trigger13@gmail.com>
2025-12-03 17:23:00 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+end
 
-         
+  
+  validates :nickname, presence: true
+  validates :firstname_kanji, presence: true
+  validates :lastname_kanji, presence: true
+  validates :firstname_kana, presence: true
+  validates :lastname_kana, presence: true
+  validates :birth_date, presence: true
+
+  has_many :items
+  has_many :orders
+
+
 end
