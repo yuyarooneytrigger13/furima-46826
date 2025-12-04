5e7283ec1174a93aa184f5fd09114bc2f6e3c044
yuyarooneytrigger13 <yuya.rooney.trigger13@gmail.com>
2025-12-04 17:51:40 +0900

    itemモデル作成、activestorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,18 @@
 class ItemsController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:new]
   def index
+
+  end
+
+  def new
+   @item = Item.new
+  end
+
+  def create
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :content, :price, :category_id, :condition_id, :shipping_fee_payer_id, :prefecture_id, :schedule_delivery_id, :image).merge(user_id: current_user.id)
   end
 end
