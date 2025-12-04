e042c4f1cd4f7cf34b2421978cd809b07fef9196
yuyarooneytrigger13 <yuya.rooney.trigger13@gmail.com>
2025-12-04 19:08:46 +0900

    astiveHash、itemモデルバリデーション

--- Gemfile
+++ Gemfile
@@ -80,4 +80,5 @@ end
 gem 'devise'
 gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
+gem 'active_hash'
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee_payer
+  belongs_to :schedule_delivery
+
+  #belongs_to :user
+  #has_one :order
+
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :price, presence: true
+
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_payer_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :schedule_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+  
+
 end
